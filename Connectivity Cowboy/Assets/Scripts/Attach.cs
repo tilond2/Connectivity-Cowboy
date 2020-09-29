2143480999a43071b6865828e81ace26cbc67950
Sir-Ross <43891611+Sir-Ross@users.noreply.github.com>
2020-09-29 12:41:17 -0400

    Made Person Ropeable, need to add attaching

---
+++ Connectivity Cowboy/Assets/Scripts/Attach.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attach : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject roped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        roped = collision.gameObject;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        roped = collision.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
