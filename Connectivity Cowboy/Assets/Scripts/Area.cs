72aec2b41b3de2cda000524c727c20ad4efb4a51
Tilon Dixon <tilond2@gmail.com>
2020-09-29 22:35:38 -0400

    Extra scene

---
+++ Connectivity Cowboy/Assets/Scripts/Area.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Area : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<GameObject> people;
+    private GameManager gm;
+    
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        people = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject person = collision.gameObject;
+            people.Add(person);
+        }
+    }
+
+
+    IEnumerator Talking()
+    {
+        yield return null;
+    }
+}
---
+++ Connectivity Cowboy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int score;
+    private Text scoreCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreCount = GameObject.Find("Score").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int s)
+    {
+        score += s;
+    }
+
+
+}
