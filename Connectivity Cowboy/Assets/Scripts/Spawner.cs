99cf6e1a0c6de2a300555bada533279de287860f
Sir-Ross <43891611+Sir-Ross@users.noreply.github.com>
2020-09-30 01:12:39 -0400

    Added initial spawner code

---
+++ Connectivity Cowboy/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    //12
+
+    // 3 0
+
+    [SerializeField]
+    public GameObject person;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
